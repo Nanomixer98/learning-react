a19e8fcd1b89a33bd1ce7f8043362f857d5c4ccb
Alexis Nava Sánchez <47218472+Nanomixer98@users.noreply.github.com>
2025-11-06 22:33:27 -0600

    Refactor GifsApp to use custom hook for GIFs management and add MyCounterApp component with counter functionality (#2)

---
+++ 02-gifs-app/src/counter/hooks/useCounter.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export const useCounter = (initialValue: number = 5) => {
+  const [counter, setCounter] = useState(initialValue);
+
+  const handleAdd = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleSubstract = () => {
+    setCounter((prevState) => prevState - 1);
+  };
+
+  const handleReset = () => {
+    setCounter(initialValue);
+  };
+
+  return {
+    // Properties
+    counter,
+    // Methods / Actions
+    handleAdd,
+    handleReset,
+    handleSubstract,
+  };
+};
